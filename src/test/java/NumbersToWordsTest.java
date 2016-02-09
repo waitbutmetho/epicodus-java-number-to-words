ca78452d674f1051c8a9cbf8388ca2ee3d828c43
Joshua Gustafson and tyler suderman <silversevenfox@yahoo.com>
2016-02-09 15:20:43 -0800

    create initial project folder for numberstowords

---
+++ src/test/java/NumbersToWordsTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class NumbersToWordsTest {
+
+  // @Test
+  // public void scrabbleScoreOfLetter_returnsAScrabbleScoreForALetter_1() {
+  //   Integer score = 1;
+  //   assertEquals(score, Scrabble.scrabbleScoreOfLetter("a"));
+  // }
+}
