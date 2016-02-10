94457eb7c441e7579d552a23ba6b622b2d9ce532
Joshua Gustafson and tyler suderman <silversevenfox@yahoo.com>
2016-02-09 16:33:54 -0800

    wrote spec for numbers to 9 and refactored

--- src/main/java/NumbersToWords.java
+++ src/main/java/NumbersToWords.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -7,52 +8,32 @@ import static spark.Spark.*;
 public class NumbersToWords {
   public static void main(String[] args) {}
 
-// public static HashMap<String, Integer> createAndPopulateHashMap() {
-//   HashMap<String, Integer> letterValues = new HashMap();
-//   letterValues.put("a", 1);
-//   letterValues.put("e", 1);
-//   letterValues.put("i", 1);
-//   letterValues.put("o", 1);
-//   letterValues.put("u", 1);
-//   letterValues.put("l", 1);
-//   letterValues.put("n", 1);
-//   letterValues.put("r", 1);
-//   letterValues.put("s", 1);
-//   letterValues.put("t", 1);
-//   letterValues.put("d", 2);
-//   letterValues.put("g", 2);
-//   letterValues.put("b", 3);
-//   letterValues.put("c", 3);
-//   letterValues.put("m", 3);
-//   letterValues.put("p", 3);
-//   letterValues.put("f", 4);
-//   letterValues.put("h", 4);
-//   letterValues.put("v", 4);
-//   letterValues.put("w", 4);
-//   letterValues.put("y", 4);
-//   letterValues.put("k", 5);
-//   letterValues.put("j", 8);
-//   letterValues.put("x", 8);
-//   letterValues.put("q", 10);
-//   letterValues.put("z", 10);
-//   return letterValues;
-// }
-//
-//   public static Integer scrabbleScoreOfLetter(String letter) {
-//     HashMap<String, Integer> letterValues = createAndPopulateHashMap();
-//     Integer value = letterValues.get(letter);
-//     return value;
-//   }
-//
-//   public static Integer scrabbleScore(String word) {
-//     String lowerCaseWord = word.toLowerCase();
-//     String[] lettersArray = lowerCaseWord.split("");
-//     Integer wordScore = 0;
-//     for(Integer i = 0; i < lettersArray.length; i++) {
-//       Integer letterScore = scrabbleScoreOfLetter(lettersArray[i]);
-//       wordScore += letterScore;
-//     }
-//
-//     return wordScore;
-  // }
+  public static String numbersToWords(Integer number) {
+    String stringNumber = number.toString();
+    String[] stringNumberArray = stringNumber.split("");
+
+
+    HashMap<String, String> onesAndTeens = new HashMap<String, String>();
+    // HashMap<String> tens = new HashMap<String>();
+    // HashMap<String> hundreds = new HashMap<String>();
+    String[] onesAndTeensWords = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    // String[] tensWords = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    // findHundredsWords.put(0, "");
+    for(Integer i = 1; i <= 19; i++) {
+      onesAndTeens.put(i.toString(), onesAndTeensWords[i-1]);
+    }
+    // for(Integer i = 2; i <= 9; i++) {
+    //   findTensWords.put(i, tensWords[i-2]);
+    // }
+    // for(Integer i = 1; i <= 9; i++) {
+    //   findHundredsWords.put(i, (smallNumberWords[i-1] + " hundred"));
+    // } //END HASHMAP SETUP
+
+    Integer indexOfWord = stringNumberArray.length - 1;
+    String hashKey = stringNumberArray[indexOfWord];
+    String word = onesAndTeens.get(hashKey);
+
+    return word;
+  }
+
 }
--- src/test/java/NumbersToWordsTest.java
+++ src/test/java/NumbersToWordsTest.java
@@ -3,9 +3,9 @@ import static org.junit.Assert.*;
 
 public class NumbersToWordsTest {
 
-  // @Test
-  // public void scrabbleScoreOfLetter_returnsAScrabbleScoreForALetter_1() {
-  //   Integer score = 1;
-  //   assertEquals(score, Scrabble.scrabbleScoreOfLetter("a"));
-  // }
+  @Test
+  public void numbersToWords_returnsAWordForANumber_String() {
+    String word = "nine";
+    assertEquals(word, NumbersToWords.numbersToWords(9));
+  }
 }
